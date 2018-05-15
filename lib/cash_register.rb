e05e84175d917c5319f526403d387ebd6f68a74f
Megan Collier <meganraec@live.com>
2018-05-15 01:41:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,51 @@
+# describe 'CashRegister' do
+#   let(:cash_register) { CashRegister.new }
+#   let(:cash_register_with_discount) { CashRegister.new(20) }
 
+class CashRegister
+  attr_accessor :total, :discount, :new_register
+
+  def initialize( discount = 0 )
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    self.total += (quantity * price)
+    quantity.times {@items << item}
+    @items << item
+  end
+
+  def apply_discount
+    if self.discount > 0
+      @total -= (@total * (@discount.to_f/100.0))
+      "After the discount, the total comes to $#{(@total.to_i)}."
+    elsif self.discount = 0
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    # self.items
+  end
+end
+
+
+#
+#   describe '#items' do
+#     it 'returns an array containing all items that have been added' do
+#       new_register = CashRegister.new
+#       new_register.add_item("eggs", 1.99)
+#       new_register.add_item("tomato", 1.76, 3)
+#       expect(new_register.items).to eq(["eggs", "tomato", "tomato", "tomato"])
+#     end
+#   end
+#
+#   describe '#void_last_transaction' do
+#     it 'subtracts the last transaction from the total' do
+#       cash_register.add_item("tomato", 1.76)
+#       expect{cash_register.void_last_transaction}.to change{cash_register.total}.from(1.76).to(0.0)
+#     end
+#   end
+# end
